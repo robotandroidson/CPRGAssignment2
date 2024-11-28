9e37dec4f2482d9398f886dca9af288d7815a943
robotandroidson <robert.anderson1@edu.sait.ca>
2024-11-27 20:09:56 -0700

    fixed broken links

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ first.addEventListener('click',function(){
     first.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="The first Tour de France was planned as a marketing ploy by the newspaper L’Auto to increase readership devised by editor Henri Desgrange. It was originally planned to be a 6 day race held between May 31-July 5, 1903 as a 5 stage race through Lyon, Marseille, Bordeaux, Nantes and ending in Paris. However, low enrolment by cyclists inspired Desgrange to change it to 6 stages (adding Toulouse), increasing the length of time to complete the race to 18 days, and increasing the prize purse to be 6 times the amount an average worker would earn in one year. With the increased enrolment, the race went ahead from July 1-July 19 1903. The race was exceedingly difficult, and many riders dropped out after the initial stages. Maurice Garin held first position through most of the race and ended up winning the first tour. In the end, circulation for L’Auto doubled during the race, and Desgrange was successful in promoting the newspaper with this race.";
 
- document.getElementById('caption').innerHTML="Maurice Garin, winner of the first Tour de France. Source: Unknown Author. Wielrennen, Tour de France 1903. Public domain, via Wikimedia Commons"+" <a href='+https://commons.wikimedia.org/wiki/File:Wielrennen,_Tour_de_France_1903,_SFA001006411.jpg'>Image Source</a>";
+ document.getElementById('caption').innerHTML="Maurice Garin, winner of the first Tour de France. Source: Unknown Author. Wielrennen, Tour de France 1903. Public domain, via Wikimedia Commons"+" <a href='https://commons.wikimedia.org/wiki/File:Wielrennen,_Tour_de_France_1903,_SFA001006411.jpg'>Image Source</a>";
 
 
 
@@ -31,7 +31,7 @@ second.addEventListener('click',function(){
 
     second.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="In 1904, the second race was held. However, this time around, cheating and fighting were rampant through the race and even the first winner, Maurice Garin, was disqualified. It was due to this behaviour that Desgrange changed the format of the race to a points system from 1906-1912. It did not matter how fast or slow you finished a stage, it mattered your place in the peloton. In this way, riders rode together at a relaxed pace until close to the finish line, this would later be coined “bicycle stage racing”. In 1910, mountain stages were introduced which greatly increased difficulty. Cyclists were forced to mend their own bicycles and were required to use the same one for the entirety of the race. In 1930, Desgrange finally allowed cyclists to have their bikes mended by a team and were allowed to change bicycles if broken. Desgrange attempted many methods for levelling the playing field such as introducing regional teams that were made up of cyclists from all levels, banning gears, banning metal rims, and even suggesting he provide the same bicycle for each rider. Desgrange died in 1940 and the race organization was taken over by Jacques Goddet. In 1939, the race was interrupted by World War 2 and did not return until 1947.";
-    document.getElementById('caption').innerHTML="1936 Tour de France. Source: Agence Rol, Public domain, via Wikimedia Commons"+" <a href='+https://commons.wikimedia.org/wiki/File:Btv1b8442891c-p21_(2).jpg'>Image Source</a>";
+    document.getElementById('caption').innerHTML="1936 Tour de France. Source: Agence Rol, Public domain, via Wikimedia Commons"+" <a href='https://commons.wikimedia.org/wiki/File:Btv1b8442891c-p21_(2).jpg'>Image Source</a>";
 
 })
 
@@ -49,7 +49,7 @@ third.addEventListener('click',function(){
     third.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="When the tour began again in 1947, the race had changed to 20-25 stages and introduced “split” stages to improve rest times. The Green Jersey points system was implemented in 1953. National teams were now allowed entry, resulting in greater worldwide excitement and coverage of the race. By the 1960s, there was a reduction in popularity of cycling and the industry was threatened to fold. Due to this, the race was allowed to be promoted by bicycle manufacturers to market their products. Therefore “trade” teams were allowed rather than national teams, meaning a team composed of many nationalities would be sponsored by a corporation. Jacques Anquetil was the first rider to win the race 5 times in 1957, and 1961-1964. Into the 1960s, doping became a prominent issue and a rider named Tom Simpson even died from the side effects in 1967. To resolve the doping issue, stages had daily kilometre limits, more rest days, and drug testing. ";
 
-    document.getElementById('caption').innerHTML="Jacques Anquetil (centre), Raymond Poulidor (left), and Federico Bahamontes (right), 1964 Tour de France podium. Source: [onbekend], CC0, via Wikimedia Commons"+" <a href='+  https://upload.wikimedia.org/wikipedia/commons/b/b2/Raymond_Poulidor%2C_Jacques_Anquetil_and_Federico_Bahamontes_podium%2C_Tour_de_France_1964_%28cropped%29.jpg'>Image Source</a>";
+    document.getElementById('caption').innerHTML="Jacques Anquetil (centre), Raymond Poulidor (left), and Federico Bahamontes (right), 1964 Tour de France podium. Source: [onbekend], CC0, via Wikimedia Commons"+" <a href='https://upload.wikimedia.org/wikipedia/commons/b/b2/Raymond_Poulidor%2C_Jacques_Anquetil_and_Federico_Bahamontes_podium%2C_Tour_de_France_1964_%28cropped%29.jpg'>Image Source</a>";
 
 })
 
@@ -67,7 +67,7 @@ fourth.addEventListener('click',function(){
     fourth.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="Eddie Merckx came on the scene in the early 1970s and dominated the peloton winning the race 6 times in between 1969 and 1974. He even set stage victory records that would not be beat until 2024 by Mark Cavendish. In 1975, the iconic polka dot jersey was implemented for mountain stage winners. During this period, the race increased their sponsorship so that they could provide more prizes than just cash. Bernard Hinault became the new superstar winning 5 times between 1977 and 1986. In 1984, a women’s version of the race called Tour de France Femenin which was won by Marianne Martin.It was in the mid 1980s that the race faced financial issues and organizers even attempted to hold a tour in the USA called Tour de America which was a financial failure.  After many changes of leadership, the race remained in financial ruin until 1987.";
 
-    document.getElementById('caption').innerHTML="Bernard Hinault at the 1978 Tour de France. Source: Unknown Author, CC BY-SA 3.0 NL, via Wikimedia Commons"+" <a href='+ https://upload.wikimedia.org/wikipedia/commons/9/94/Bernard_Hinault_1978.jpg'>Image Source</a>";
+    document.getElementById('caption').innerHTML="Bernard Hinault at the 1978 Tour de France. Source: Unknown Author, CC BY-SA 3.0 NL, via Wikimedia Commons"+" <a href='https://upload.wikimedia.org/wikipedia/commons/9/94/Bernard_Hinault_1978.jpg'>Image Source</a>";
 })
 
 fifth.addEventListener('click',function(){
@@ -84,7 +84,7 @@ fifth.addEventListener('click',function(){
     fifth.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="It was in this time period that became known as the doping era. Many riders cheated with performance enhancing drug named erythropoietin (EPO) which did not show up on tests at the time. Miguel Indurain won the race 5 times between 1991-1995, the only rider to do so consecutively in the tour’s history due to his strong time-trial performances and strong lead through several stages in each race. More international riders began to compete in 1996 and Danish rider Bjarne Riis dethroned Indurain. However this would be a dishonest win as he admitted to doping. Due to this he would lose his classification as winner, but in 2008 it was reinstated with an asterisk to his name indicating doping.";
 
-    document.getElementById('caption').innerHTML="Miguel Indurain at the 1993 Tour de France. Source: Eric Houdas, Miguel Indurain, CC BY-SA 3.0 NL, via Wikimedia Commons"+" <a href='+ https://upload.wikimedia.org/wikipedia/commons/b/b0/Miguel_INDURAIN_%28cropped%29.jpg'>Image Source</a>";
+    document.getElementById('caption').innerHTML="Miguel Indurain at the 1993 Tour de France. Source: Eric Houdas, Miguel Indurain, CC BY-SA 3.0 NL, via Wikimedia Commons"+" <a href='https://upload.wikimedia.org/wikipedia/commons/b/b0/Miguel_INDURAIN_%28cropped%29.jpg'>Image Source</a>";
   
 
 })
@@ -103,7 +103,7 @@ sixth.addEventListener('click',function(){
     sixth.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="The doping problem did not end in this time period and systematic doping became apparent and known as the Festina Affair. Many teams and riders dropped out due to this and in 1998 a highly stripped competitive field allowed Marco Pantani to pull off a win. In 1999, the tour tried to revive the image of the sport deeming this race the “Tour of Renewal”. However, this would not be the case as Lance Armstrong, miraculously winning the tour 7 times consecutively between 1999-2006 despite beating cancer, would be doping this entire time. Armstrong’s wins were revoked and it states as “No Winner” in historical records.";
 
-    document.getElementById('caption').innerHTML="Andy Schleck (left) and Alberto Contador (right) at the 2009 Tour de France. Source: filip bossuyt from Kortrijk, Belgium, Tour de France 2009, CC BY 2.0, via Wikimedia Commons"+" <a href='+ https://upload.wikimedia.org/wikipedia/commons/6/6a/Tour_de_France_2009%2C_andy_en_albert_%2822014224710%29.jpg'>Image Source</a>";
+    document.getElementById('caption').innerHTML="Andy Schleck (left) and Alberto Contador (right) at the 2009 Tour de France. Source: filip bossuyt from Kortrijk, Belgium, Tour de France 2009, CC BY 2.0, via Wikimedia Commons"+" <a href='https://upload.wikimedia.org/wikipedia/commons/6/6a/Tour_de_France_2009%2C_andy_en_albert_%2822014224710%29.jpg'>Image Source</a>";
 
 })
 
@@ -120,5 +120,5 @@ final.addEventListener('click',function(){
 
     final.style.backgroundColor = '#97bf04';
     document.getElementById('changep').textContent="From 2012 to present, Team Sky has dominated the tour with wins by Chris Froome, Bradley Wiggins, Geraint Thomas, and Egan Bernal. Bernal would become the first Colombian winner in 2019. Due to COVID-19, the race was held in late August of 2020, the first time since the end of WW2 it was not in July. Tadej Pogacar came on top in 2020 and 2021 and would also be the first Slovenian winner, and second youngest winner at 21 years old. In 2022 and 2023 Jonas Vingegaard would break Pagacar’s winning streak. In 2022, the women’s version returned as the Tour de France Femmes, the last being in 1989. In 2024, Pogacar returned to the top spot and remains a powerhouse in the tour to this day.";
-    document.getElementById('caption').innerHTML="Tadej Pogacar (right) and Jonas Vingegaard (left) during the 2022 Tour de France. Source: filip bossuyt from Kortrijk, Belgium, CC BY 2.0 vie Wikimedia Commons"+" <a href='+ https://upload.wikimedia.org/wikipedia/commons/b/b7/TDF3033_pogacar_vingegaard_%2852243724489%29.jpg'>Image Source</a>";
+    document.getElementById('caption').innerHTML="Tadej Pogacar (right) and Jonas Vingegaard (left) during the 2022 Tour de France. Source: filip bossuyt from Kortrijk, Belgium, CC BY 2.0 vie Wikimedia Commons"+" <a href='https://upload.wikimedia.org/wikipedia/commons/b/b7/TDF3033_pogacar_vingegaard_%2852243724489%29.jpg'>Image Source</a>";
 })
